0514dbb3f9360aab55b28a07c43345df4b1a7121
Dominick Caponi <d.caponi1@gmail.com>
2019-09-02 19:41:07 -0700

    tweak rating_links_controller to allow permanent links

--- app/controllers/rating_links_controller.rb
+++ app/controllers/rating_links_controller.rb
@@ -21,10 +21,11 @@ class RatingLinksController < ApplicationController
   # GET /rating_links/1.json
   def show
     @rating_link = RatingLink
-      .where("expires_at > ?", Date.today)
       .where(uuid: params[:uuid])
+      # .where("expires_at > ?", Date.today) For now all links are permanent
+
     unless @rating_link.empty?
-      render json: @rating_link, status: :ok
+      render json: {rating_links: [@rating_link]}, status: :ok
     else
       render json: {rating_link: "the requested resource was not found"}, status: :not_found
     end
@@ -36,13 +37,14 @@ class RatingLinksController < ApplicationController
     if cookies[:login]
       jwt = JsonWebToken.decode(cookies[:login])
       if jwt
-        create_params = rating_link_params
+        create_params = {}
         create_params[:user_id] = jwt[:id]
-        create_params[:expires_at] = 96.hour.from_now
+        # TODO: Move temporary links over to redis. For now, all links are permanent
+        # create_params[:expires_at] = 96.hour.from_now
         @rating_link = RatingLink.new(create_params)
 
         if @rating_link.save
-          render json: @rating_link, status: :created
+          render json:  {rating_links: [@rating_link]}, status: :created
         else
           render json: @rating_link.errors, status: :unprocessable_entity
         end
@@ -57,23 +59,49 @@ class RatingLinksController < ApplicationController
   # PATCH/PUT /rating_links/1
   # PATCH/PUT /rating_links/1.json
   def update
-    if @rating_link.update(rating_link_params)
-      render json: @rating_link, status: :ok
+    if cookies[:login]
+      jwt = JsonWebToken.decode(cookies[:login])
+      if jwt
+        if @rating_link.update(rating_link_params)
+          render json:  {rating_links: [@rating_link]}, status: :ok
+        else
+          render json: @rating_link.errors, status: :unprocessable_entity
+        end
+      else
+        render json: {invalid_credential: "invalid credential given"}, status: 401
+      end
     else
-      render json: @rating_link.errors, status: :unprocessable_entity
+      render json: {no_credential: "no credential given"}, status: 401
     end
   end
 
   # DELETE /rating_links/1
   # DELETE /rating_links/1.json
   def destroy
-    # @rating_link.destroy
+    if cookies[:login]
+      jwt = JsonWebToken.decode(cookies[:login])
+      if jwt
+        if @rating_link
+          if @rating_link.destroy
+            render json: @rating_link, status: :ok
+          else
+            render json: @rating_link.errors, status: :unprocessable_entity
+          end
+        else
+          render json: {rating_link: "The rating link with the given uuid could not be found"}, status: 401
+        end
+      else
+        render json: {invalid_credential: "invalid credential given"}, status: 401
+      end
+    else
+      render json: {no_credential: "no credential given"}, status: 401
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_rating_link
-      @rating_link = RatingLink.find(params[:id])
+      @rating_link = RatingLink.find_by_uuid(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
